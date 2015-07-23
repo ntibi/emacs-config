7362367c9676dbdbf79bfd1a9c9905d848666121
Nicolas TIBI <ntibi@e3r9p10.42.fr>
2015-07-23 14:21:52 +0200

    working install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+PWD = pwd
+
 if [ -f ~/.emacs ];
 then
 	mv ~/.emacs /tmp
 fi
-ln -s ./.emacs ~/.emacs
+ln -sF ~/emacs-config/.emacs ~/.emacs
 
 if [ -d ~/.emacs.d/lisp ];
 then
 	mv ~/.emacs.d/lisp /tmp
 fi
-ln -s ./.emacs.d/lisp/ ~/.emacs.d/lisp
+ln -sF ~/emacs-config/.emacs.d/lisp/ ~/.emacs.d/lisp
 
 if [ -d ~/.emacs.d/snippets ];
 then
 	mv ~/.emacs.d/snippets /tmp
 fi
-ln -s ./.emacs.d/snippets/ ~/.emacs.d/snippets
+ln -sF ~/emacs-config/.emacs.d/snippets/ ~/.emacs.d/snippets
