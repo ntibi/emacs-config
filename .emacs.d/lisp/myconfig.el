6a0922a107fa4cc128ee4784cc3c2c3fbfa6a230
Nicolas TIBI <ntibi@e3r9p13.42.fr>
2015-07-27 17:28:39 +0200

    new key to (un)comment

--- .emacs.d/lisp/myconfig.el
+++ .emacs.d/lisp/myconfig.el
@@ -38,6 +38,7 @@
 (global-set-key (kbd "C-c d") 'duplicate-line-or-region) ; explicit name ;)
 (global-set-key (kbd "C-x c") 'copy-line)				 ; copy line
 (global-set-key (kbd "C-c c") 'comment-or-uncomment-region-or-line)
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region-or-line)
 
 (global-set-key (kbd "C-c r") 'reload-dotemacs-file) ; reload emacs config
 
